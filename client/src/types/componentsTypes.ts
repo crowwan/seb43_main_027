72659f0c388da3b6727a0d74a0399909cdadcd6d
crowwan <crowwan@naver.com>
2023-05-19 14:57:33 +0900

    :lipstick: style: 세미콜론추가

--- client/src/types/componentsTypes.ts
+++ client/src/types/componentsTypes.ts
@@ -1,31 +1,31 @@
 import { useInputReturn } from './customHooksTypes';
 
 export interface ButtonType {
-  flex?: string,
-  fontSize?: string,
-  fontWeight?: string,
-  bg?: string,
-  fontColor?: string,
-  margin?: string,
-  padding?: string,  
-  radius?: string,
-  border?: string,
-  hoverBg?: string,
+  flex?: string;
+  fontSize?: string;
+  fontWeight?: string;
+  bg?: string;
+  fontColor?: string;
+  margin?: string;
+  padding?: string;
+  radius?: string;
+  border?: string;
+  hoverBg?: string;
 }
 
 export interface InputType {
-  fontSize?: string,
-  fontWeight?: string,
-  bg?: string,
-  fontColor?: string,
-  margin?: string,
-  padding?: string,  
-  radius?: string,
-  border?: string,
-  hoverBg?: string,
-  boxShadow?: string,
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void | undefined,
-  useInput?: useInputReturn
+  fontSize?: string;
+  fontWeight?: string;
+  bg?: string;
+  fontColor?: string;
+  margin?: string;
+  padding?: string;
+  radius?: string;
+  border?: string;
+  hoverBg?: string;
+  boxShadow?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void | undefined;
+  useInput?: useInputReturn;
 }
 
 export interface InputContainerType {
@@ -33,11 +33,11 @@ export interface InputContainerType {
   title?: string;
   onChange?: React.ChangeEvent;
   validationMessage?: string;
-  extraAction?: (value:string) => void;
+  extraAction?: (value: string) => void;
   validationFunction?:
     | ((value: string) => boolean)
     | ((value: File) => boolean);
-  type?: string; 
+  type?: string;
 }
 
 export interface LabelType {
